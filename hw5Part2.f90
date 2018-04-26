8c7b2f8bf3575d417edaf0e9145d1326bd330d0a
Fars_alfawzan <fars_alfawzan@titanv.acad.fullerton.edu>
2018-04-25 18:35:02 -0700

    changes in printings

--- hw5Part2.f90
+++ hw5Part2.f90
@@ -8,28 +8,40 @@ integer :: check
 
 A = 0
 A = A + 5
-
+print*, "added 5 to matrix A"
+print*, "printing matrix A"
+do i = 1, 4 , 1
+        do j = 1, 4, 1
+                print*, A(i,j)
+end do
+end do
+print*, "________________"
+print*, "Printing Matrix B after i+j"
 do i = 1, 4 , 1
 	do j = 1, 4, 1
 		B(i,j) = i + j
 		print*, B(i,j)
 end do 
 end do
-
+print*,"________________"
+print*,"Adding A+B to C"
 C = A + B
+print*,"Printing Matrix C"
 do i = 1, 4, 1
 	print*, ""
 	do j = 1, 4, 1
-		write(stdout, 10, advance='no') A(i,j)
+		print*, C(i,j)
 	end do
 end do
-
+print*,"_________________"
+print*,"Replacing 8 with 18 in matrix C and printing it"
 do i = 1, 4, 1
 	do j = 1, 4, 1
 		check = C(i,j)
 		if (check == 8) then
+			print*, "replacing elements in subscript",i,j,"with 18"
 			C(i,j) = 18
-			print*, C(i,j)
+			
 		end if
 	end do
 end do
